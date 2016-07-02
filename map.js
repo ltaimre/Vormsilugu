dcdc3250663bc2e69e3a586cfef71efcc83d5b1c
ltaimre <liisitaimre@gmail.com>
2016-07-02 23:36:06 +0300

    kaart ja fotode asukohad

---
+++ map.js
@@ -0,0 +1,7 @@
+  var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 59.0038222, lng: 23.1821927},
+          zoom: 12
+        });
+      }
